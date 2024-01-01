bab22d2cbb5046d6e5e3c40a4fc4eb83105a29ed
Supermarcel10 <marcelbarlik@gmail.com>
2024-01-01 16:57:41 +0000

    - Ported to React.js
    Took 30 minutes

---
+++ old/ParseYAML.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+try {
+	const fileContents = fs.readFileSync('../data/year2.yaml', 'utf8');
+	const data = yaml.load(fileContents);
+	console.log(data);
+} catch (e) {
+  console.error(e);
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+		{/*Title*/}
+		<h1>Grade Calculator</h1>
+		<p id="caption">So you can show off to your friends!</p>
+		<br/>
+
+		{/*TODO: Calculator*/}
+  </React.StrictMode>
+);
