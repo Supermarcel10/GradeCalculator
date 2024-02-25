583467967e8913fe4e83bb896248d0452beb762c
Supermarcel10 <marcelbarlik@gmail.com>
2024-02-25 23:34:46 +0000

    - Updated input sizes to be more responsive for smaller sizes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 		<h1 className="text-center font-bold text-4xl pt-8">Grade Calculator</h1>
 		<p className="text-center italic pb-12">So you can show off to your friends!</p>
 
-		<div className="mx-auto flex flex-col lg:flex-row gap-4 lg:gap-8 lg:px-8 pb-16 items-center justify-center">
+		<div className="mx-auto flex flex-col lg:flex-row gap-4 lg:gap-8 lg:px-12 pb-16 items-center justify-center">
 			<label className="form-control w-3/5 md:w-full md:max-w-md">
 				<div className="label">
 					<span className="label-text">University</span>
